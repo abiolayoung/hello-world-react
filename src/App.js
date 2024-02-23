d53ec7764b6821642139073f5bd5cff627dc7761
Abiolayoung <abiolayoung229@gmail.com>
2024-02-24 00:56:51 +0100

    hello-world-react-app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 id="head">Hello World!</h1>
+     <div class="flex">
+       <p>Working in the technology sector has always been my priority</p>
+       <p>Working in the aviation sector has always been my priority</p>
+     </div>
+    </div>
+  );
+}
+
+export default App;
