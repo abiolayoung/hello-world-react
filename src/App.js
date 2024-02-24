424c61b3d5126e2d5e5a7aeb838707dcd4187ffb
Abiolayoung <abiolayoung229@gmail.com>
2024-02-24 02:25:34 +0100

    nice

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
-// import logo from './logo.svg';
+import logo from './assets/logo.jpg';
+import abiola from './assets/abiola.jpeg';
 import './App.css';
 import React from 'react';
 
 // creating a react component
 function Greetings() {
-  return <p>hello world, my name is abeeb abiola adeniyi from altschool africa</p>
+  return <h2>Mindlift Wellness Staff Profile</h2>
 }
 
+//creating single component profile card...
 function UserProfileCard (){
   return (
     <div className="card">
-    <div className="card-name">Adeniyi Abiola</div>
+      <img className="card-img"
+    src={abiola} alt="logo- image"/>
+    <div className="card-name">Adeniyi Abiola (Co-Founder)</div>
     <div>Abiola is a lovely and a tech enthusiast, schooling at altschool</div>
-    <img className="card-img"
-    src=""/>
     </div>
   )
 }
+
+// spliting components
+
+function Avatar(){
+  return <img className="card-img"
+  src={logo} alt="logo- image"/>
+}
+
+function UserProfileCard2 (){
+  return (
+    <div className="card">
+    <Avatar/>
+    <div className="card-name">Salvation Okafor (Development Specialist)</div>
+    <div>Salvation is a lovely and a tech enthusiast, schooling at altschool</div>
+    </div>
+  )
+}
+
 // root component
 function App() {
   return <section className="main-page">
-    <Greetings/>
+    <div class="header"><Greetings/></div>
     <UserProfileCard/>
+    <UserProfileCard2/>
   </section>
   
 }
